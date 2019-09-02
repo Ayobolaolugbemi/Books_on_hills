9c0416d975ee53063af65db4f7f08ccf4cac5e08
Ayobolaolugbemi <46406626+Ayobolaolugbemi@users.noreply.github.com>
2019-09-01 19:19:42 -0500

    Updated values

--- TestGitProject/src/Additiontest/Addition.java
+++ TestGitProject/src/Additiontest/Addition.java
@@ -5,8 +5,8 @@ public class Addition {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int a =10;
-		int b= 20;
+		int a =100;
+		int b= 200;
 		int c = a+b;
 		int d = 35;
 		int e = c+d;
